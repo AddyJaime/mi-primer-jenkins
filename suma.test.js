d94cb5e1aa3cc930a721bb81194f8540e09b4cc9
addy <addyjaime@gmail.com>
2026-05-09 19:55:32 -0400

    pruebo trigger sin .git

--- suma.test.js
+++ suma.test.js
@@ -1,7 +1,7 @@
 const suma = require("./suma");
 
 if (suma(2, 3) === 5) {
-  console.log("✅ Prueba pasada! - Trigger funcionando");
+  console.log("✅ Prueba pasada! - Trigger funcionando, probando");
 
   process.exit(0);
 } else {
