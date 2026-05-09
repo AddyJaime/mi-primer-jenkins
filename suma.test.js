cbf556553560bbcebeb831a2058d328f150e8073
addy <addyjaime@gmail.com>
2026-05-09 19:36:17 -0400

    probar trigger automatico

--- suma.test.js
+++ suma.test.js
@@ -1,9 +1,10 @@
 const suma = require("./suma");
 
-if (suma(2,3)=== 5) {
-  console.log("Prueba pasada")
-  process.exit(0)
+if (suma(2, 3) === 5) {
+  console.log("✅ Prueba pasada! - Trigger funcionando");
+
+  process.exit(0);
 } else {
-  console.log("Prueba fallida")
-  process.exit(1)
+  console.log("Prueba fallida");
+  process.exit(1);
 }
